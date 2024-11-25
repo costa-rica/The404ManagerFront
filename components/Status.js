d152d7e553828ab78763aa15f654c5dd3524b511
Nick <nrodrig1@gmail.com>
2024-11-25 12:59:21 +0100

    table works with static data

--- components/Status.js
+++ components/Status.js
@@ -1,68 +1,120 @@
 import styles from "../styles/Status.module.css";
 import { useState, useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { loginUser } from "../reducers/user";
-import { useRouter } from "next/router";
 import HeaderCustom from "./Header";
-import StatusTableRow from "./subComponents/StatusTableRow";
 
 export default function Status() {
-  const [appsList, appsListSetter] = useState([]);
+  // const [appsList, appsListSetter] = useState([]);
+  const [appsList, appsListSetter] = useState(fauxReponse.appsList);
 
-  const dispatch = useDispatch();
-  const router = useRouter();
-  const user = useSelector((state) => state.user.value);
+  const [appListRows, appListRowsSetter] = useState([]);
 
-  useEffect(() => {
-    (async () => {
-      try {
-        const response = await fetch(
-          `${process.env.NEXT_PUBLIC_API_BASE_URL}/status/list-apps`,
-          {
-            method: "GET",
-            headers: {
-              "Content-Type": "application/json",
-              Authorization: `Bearer ${user.token}`, // Add token to Authorization header
-            },
-          }
-        );
-        const responseJson = await response.json();
-        console.log(responseJson);
+  // useEffect(() => {
+  //   const appsListTemp = [];
+  //   appsList.map((elem, index) => {
+  //     let keyVal = `tr${index}`;
+  //     console.log(keyVal);
+  //     const stuff = (
+  //       <tr key={keyVal}>
+  //         <td>
+  //           <span style={styles2.spanAppName}>{elem.name}</span>
+  //           <br />
+  //           <span style={styles2.spanAppProjectPath}>
+  //             {elem.appProjectPath}
+  //           </span>
+  //         </td>
+  //         <td>{elem?.port}</td>
+  //         <td>
+  //           <button onClick={() => toggleStatus(elem.name)}>
+  //             {elem.status == "online" ? "active" : "inactive"}
+  //           </button>
+  //         </td>
+  //       </tr>
+  //     );
 
-        appsListSetter(responseJson.appsList);
-      } catch {
-        console.error("Error fetching data:");
-        dispatch(setMachineNameRedux("failed to get API response"));
-      }
-    })();
-  }, []);
+  //     appsListTemp.push(stuff);
+  //   });
+  //   appListRowsSetter(appsListTemp);
+  // }, []);
   const toggleStatus = (appName) => {
     console.log(`- in toggleStatus: ${appName}`);
   };
-  const appListRows = [];
-  appsList.map((elem, index) => {
-    const stuff = (
-      <tr>
-        <StatusTableRow elem={elem} />
-      </tr>
-    );
-
-    appListRows.push(stuff);
-  });
 
   return (
     <main className={styles.mainStatus}>
       <HeaderCustom />
       <div className="restOfPage">
         <table className={styles.tableStyle}>
-          <tr>
-            <th>App name</th>
-            <th>Port</th>
-            <th>Status</th>
-          </tr>
-          {appListRows}
+          <thead>
+            <tr>
+              <th>App name</th>
+              <th>Port</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            {appsList.map((elem) => (
+              <tr key={elem.id}>
+                <td>
+                  <span style={styles2.spanAppName}>{elem.name}</span>
+                  <br />
+                  <span style={styles2.spanAppProjectPath}>
+                    {elem.appProjectPath}
+                  </span>
+                </td>
+                <td>{elem?.port || "N/A"}</td>
+                <td>
+                  <button onClick={() => toggleStatus(elem.name)}>
+                    {elem.status === "online" ? "active" : "inactive"}
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
         </table>
       </div>
     </main>
   );
 }
+
+const fauxReponse = {
+  result: true,
+  appsList: [
+    {
+      id: 0,
+      name: "The404ManagerBack",
+      status: "online",
+      port: 8000,
+      appProjectPath: "/home/dashanddata_user/applications/The404ManagerBack",
+    },
+    {
+      id: 1,
+      name: "DevelopmentWebApp",
+      status: "online",
+      appProjectPath: "/home/dashanddata_user/applications/DevelopmentWebApp",
+    },
+    {
+      id: 2,
+      name: "DevelopmentWebApp02",
+      status: "stopped",
+      port: 8002,
+      appProjectPath: "/home/dashanddata_user/applications/DevelopmentWebApp02",
+    },
+    {
+      id: 3,
+      name: "StockPriceModel02",
+      status: "stopped",
+      port: 8003,
+      appProjectPath: "/home/dashanddata_user/applications/StockPriceModel02",
+    },
+  ],
+};
+
+const styles2 = {
+  spanAppName: {
+    fontSize: "xx-large",
+    color: "#fa9f15",
+  },
+  spanAppProjectPath: {
+    color: "#888787",
+  },
+};
