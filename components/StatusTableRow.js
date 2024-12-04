d53af3c1eb88d664bfe613e0ad5a07ee633ce52e
Nick <nrodrig1@gmail.com>
2024-12-04 18:27:31 +0100

    <td> with additionalContent is filling up with data

--- components/StatusTableRow.js
+++ components/StatusTableRow.js
@@ -56,10 +56,36 @@ export default function StatusTableRow(props) {
 
   let counter = 0;
   for (let item of props.elem.innerListObjects) {
+    console.log("item.nodeEnv");
+    console.log(item.nodeEnv);
     listOfInnerElements.push(
       <div key={counter}>
+        <div className={styles.divAddContentTitle}>
+          Local IP Address (:port) of Machine:
+        </div>
+        <div className={styles.divAddContent}>
+          {props.elem.localIpOfMachine}:{props.elem.port}
+        </div>
+        <div className={styles.divAddContentTitle}>Machine Name:</div>
+        <div className={styles.divAddContent}>{item.machineName}</div>
+
         <div className={styles.divAddContentTitle}>filename:</div>
         <div className={styles.divAddContent}>{item.filename}</div>
+        <div className={styles.divAddContentTitle}>urls:</div>
+        {Array.from({ length: item.urls.length }, (_, index) => (
+          <div className={styles.divAddContent}>
+            {index + 1}
+            {")"} {item.urls[index]}
+          </div>
+        ))}
+        <div className={styles.divAddContentTitle}>
+          Nginx File Last Modified:
+        </div>
+        <div className={styles.divAddContent}>{item.dateFileModified}</div>
+        <div className={styles.divAddContentTitle}>
+          Database Record Last Modified:
+        </div>
+        <div className={styles.divAddContent}>{item.dateRecordModified}</div>
       </div>
     );
     counter++;
@@ -67,9 +93,17 @@ export default function StatusTableRow(props) {
 
   return (
     <>
-      <td>
+      <td className={styles.toggleCell} onClick={toggleAdditionalRowsVisible}>
         <div className={styles.tdDivNameOfApp}>{props.elem.nameOfApp}</div>
-        <div className={styles.additionalContent}></div>
+        <div
+          className={`${styles.additionalContent} ${
+            additionalRowsVisible ? styles.visible : styles.hidden
+          }`}
+        >
+          <div className={styles.divAddContentTitle}>NODE_ENV:</div>
+          <div className={styles.divAddContent}>{props.elem.nodeEnv}</div>
+          {listOfInnerElements}
+        </div>
       </td>
       <td className={styles.tdPort}>{props.elem.port}</td>
       <td className={styles.tdBtnStatus}>
